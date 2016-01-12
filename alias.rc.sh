f688f9fb2ed634dc16d7f674580af5e74bbfd724
Yang Liu <jeremyrobturtle@gmail.com>
2016-01-12 18:03:43 -0500

    alias workflowManager

--- alias.rc.sh
+++ alias.rc.sh
@@ -1,2 +1,5 @@
 # Show all branches in a git repository
 alias gitkall='for b in "`git branch`"; do echo "$b"; done | tr -d "*" | xargs gitk'
+
+# Alfred workflow management
+alias workflowManagement='awm'
