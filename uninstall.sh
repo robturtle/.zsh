d6642991c8e0062b9408d24bf2fb8465fd8539c0
Yang Liu <jeremyrobturtle@gmail.com>
2016-03-18 02:48:55 -0400

    An uninstaller script

---
+++ uninstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# For debugging, users don't use it
+
+if [[ -z "${UNINSTALLER}" ]]; then
+    echo -n "Tell me your command for removing packages (i.e. sudo apt-get remove): "
+    read uninstaller
+    export UNINSTALLER=$uninstaller
+    echo "$uninstaller" >> ~/.zsh.my.sh
+fi
+
+function remove {
+    eval `echo $UNINSTALLER $1`
+}
+
+remove zsh
+remove easy_install
+remove ranger
+pip uninstall percol
+rm `which pip`
+rm -rf "${HOME}/.oh-my-zsh"
