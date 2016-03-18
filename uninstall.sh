43148a3da36258b56e77013ba85f592d230eebfe
Yang Liu <jeremyrobturtle@gmail.com>
2016-03-18 03:51:21 -0400

    fix quotes

--- uninstall.sh
+++ uninstall.sh
@@ -4,8 +4,8 @@
 if [[ -z "${UNINSTALLER}" ]]; then
     echo -n "Tell me your command for removing packages (i.e. sudo apt-get remove): "
     read uninstaller
-    export UNINSTALLER="$uninstaller"
-    echo "$uninstaller" >> ~/.zsh.my.sh
+    echo "UNINSTALLER='$uninstaller'" >> ~/.zsh.my.sh
+    source ~/.zsh.my.sh
 fi
 
 function remove {
