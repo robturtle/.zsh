87ee88e0461903cb021db7a61e82c13b8f49b507
Yang Liu <jeremyrobturtle@gmail.com>
2016-03-19 07:49:51 -0400

    js browser-sync function
    browser-sync-start start the default static server
    browser-sync-on links a file as index.html

--- plugins/js.rc.sh
+++ plugins/js.rc.sh
@@ -5,6 +5,25 @@ function install-js {
     initinstall $1 "npm install -g $1"
 }
 
+function browser-sync-start {
+    ps | grep "[b]rowser-sync start" &> /dev/null
+    if [[ "$?" == "0" ]]; then
+        echo "browser-sync already started."
+    else
+        browser-sync start --server --files "*.html, css/*.css"
+    fi
+}
+
+function browser-sync-on {
+    if [[ -h index.html ]]; then
+        rm index.html
+    else
+        mv index.html index.html.bk
+    fi
+    ln -s $1 index.html
+    ps | grep "[b]rowser-sync start" &> /dev/null || browser-sync-start
+}
+
 # for developing (Spacemacs)
 install-js tern
 install-js js-beautify
